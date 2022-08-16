8a5d94023326ce549fd380deed47720e07e05e13
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-08-17 01:52:53 +0200

    fix: adding the backend link

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3 cp --recursive --acl public-read ./www s3://589014390805-abzo-udacity-third-project/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://589014390805-abzo-udacity-third-project/
