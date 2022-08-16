65f037d39b243db1df1be96c1f59a384608304b9
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-08-16 18:03:37 +0200

    chore: editing Json file zip command & start script

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+// it uses the default value of database port
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
