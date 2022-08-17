0e0f161ec7cca36f9f5a9df9cc45dee198d9892e
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-08-17 04:17:55 +0200

    refactor: fixing typos in Register

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udagram-api-environment.eba-z7enscqt.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
